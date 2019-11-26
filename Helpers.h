ba7669537aee6f177a541a6d7091a0cbf31e3a0f
Krists D <venom.vertigo@gmail.com>
2019-11-26 11:52:18 +0200

    Log scale fix

--- Helpers.h
+++ Helpers.h
@@ -4,19 +4,19 @@
 
 double LinToLog(double linValue, double linStart, double linEnd, double logStart, double logEnd) //Returns a corresponding logarithmic value mapped from a linear scale to a logarithmic scale
 {
-	double b = log(logEnd / logStart) / (linEnd - linStart);
-	double a = logEnd / exp(b*linEnd);
-	double logValue = a * exp(b * linValue);
+	double b = log10(logEnd / logStart) / (linEnd - linStart);
+	double a = logEnd / pow(10, b*linEnd);
+	double logValue = a * pow(10, b * linValue);
 
 	return logValue;
 }
 
 double LogToLin(double logValue, double logStart, double logEnd, double linStart, double linEnd) //Returns a corresponding linear value mapped from a logarithmic scale to a linear scale
 {
-	double a = (linEnd - linStart) / log(logEnd / logStart);
-	double b = linEnd - log(logEnd)*a;
+	double a = (linEnd - linStart) / log10(logEnd / logStart);
+	double b = linEnd - log10(logEnd)*a;
 
-	double linValue = a * log(logValue) + b;
+	double linValue = a * log10(logValue) + b;
 
 	return linValue;
 }
